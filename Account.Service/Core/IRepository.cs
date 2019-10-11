25a2a1e5d67e701963f46fa3b63c8c02ec926895
gayankanishka <gkanishka6@gmail.com>
2019-10-12 00:19:01 +0530

    Adding missed repository interface

---
+++ Account.Service/Core/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Account.Service.Core
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<bool> Add(T entity);
+        Task<T> GetById(int id);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(int id);
+    }
+}
