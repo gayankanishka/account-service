6c611de1749c6e3010eeb26b167e66f1da7ed233
gayankanishka <gkanishka6@gmail.com>
2019-10-12 13:08:43 +0530

    Implementing all of the crud operations with SP's using dapper

--- Account.Service/Data/AccountRepository.cs
+++ Account.Service/Data/AccountRepository.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Data;
 using Account.Service.Core;
-using Account.Service.Core.Models;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
+using Account.Service.Models;
 
 namespace Account.Service.Data
 {
@@ -12,7 +14,13 @@ namespace Account.Service.Data
     {
         #region Variables
 
-        private readonly IDbConnection _dbConnection;
+        public string ConnectionString { get; set; }
+
+        private const string InsertAccount = "Insert_Account";
+        private const string UpdateAccount = "Update_Account";
+        private const string DeleteAccount = "Delete_Account";
+        private const string SelectAllAccounts = "Select_All";
+        private const string SelectAccountById = "Select_Single";
 
         #endregion
 
@@ -20,39 +28,68 @@ namespace Account.Service.Data
 
         public AccountRepository(string connectionString)
         {
-            _dbConnection = new SqlConnection(connectionString);
+            ConnectionString = connectionString;
         }
 
         #endregion
 
         #region Methods
 
-        public async Task<bool> Add(AccountDto entity)
+        public async Task<bool> InsertSingle(AccountDto entity)
         {
-            
-            const string sql = "INSERT INTO [dbo].[Accounts] ([Name], [Email]) VALUES (@Name ,@Email)";
-            int count = await _dbConnection.ExecuteAsync(sql, entity);
-            return count > 0;
+            using (IDbConnection connection = CreateDbConnection())
+            {
+                int count = await connection.ExecuteAsync(InsertAccount, entity, commandType: CommandType.StoredProcedure);
+                return count > 0;
+            }
         }
 
-        public async Task<bool> Delete(int id)
+        public async Task<IEnumerable<AccountDto>> SelectAll()
         {
-            throw new System.NotImplementedException();
+            using (IDbConnection connection = CreateDbConnection())
+            {
+                return await connection.QueryAsync<AccountDto>(SelectAllAccounts, commandType: CommandType.StoredProcedure);
+            }
         }
 
-        public async Task<IEnumerable<AccountDto>> GetAll()
+        public async Task<AccountDto> SelectById(AccountDto entity)
         {
-            throw new System.NotImplementedException();
+            using (IDbConnection connection = CreateDbConnection())
+            {
+                var result = await connection.QueryAsync<AccountDto>(SelectAccountById, entity, commandType: CommandType.StoredProcedure);
+                return result?.FirstOrDefault();
+            }
         }
 
-        public async Task<AccountDto> GetById(int id)
+        public async Task<bool> UpdateSingle(AccountDto entity)
         {
-            throw new System.NotImplementedException();
+            using (var ts = new TransactionScope())
+            {
+                using (IDbConnection connection = CreateDbConnection())
+                {
+                    int count = await connection.ExecuteAsync(UpdateAccount, entity, commandType: CommandType.StoredProcedure);
+                    ts.Complete();
+                    return count > 0;
+                }
+            }
         }
 
-        public async Task<bool> Update(AccountDto entity)
+        public async Task<bool> DeleteSingle(AccountDto entity)
         {
-            throw new System.NotImplementedException();
+            using (var ts = new TransactionScope())
+            {
+                using (IDbConnection connection = CreateDbConnection())
+                {
+                    int count = await connection.ExecuteAsync(DeleteAccount, entity, commandType: CommandType.StoredProcedure);
+                    ts.Complete();
+                    return count > 0;
+                }
+            }
+        }
+
+        private IDbConnection CreateDbConnection()
+        {
+            return new SqlConnection(ConnectionString);
         }
 
         #endregion
